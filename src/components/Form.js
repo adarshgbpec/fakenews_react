6f3a0722ccea029759b8a619da381d16249d5515
adarshgbpec <63949396+adarshgbpec@users.noreply.github.com>
2021-05-15 14:09:28 +0530

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Styles/form_style.css'
+
+function Form()
+{
+
+
+
+return(
+
+<div className="input_form">
+<form>
+
+  <textarea></textarea>
+  <button type="button" ><i className="fa fa-search"></i></button>
+
+
+</form>
+
+</div>
+)
+}
+
+
+
+
+
+export default Form;
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React,{useEffect} from "react"
+import './Styles/todo_style.css'
+function Todo({todo,addtodo,ftodos})
+{
+
+return(
+<div className="todolist">
+False
+</div>
+)
+}
+export default Todo
