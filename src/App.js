4da0fd8b2488a082197d44bd86816a64cd1616dc
adarshgbpec <63949396+adarshgbpec@users.noreply.github.com>
2021-05-15 14:04:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Form from './components/Form'
+import Todo from './components/Todo'
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+  <h1 className="heading">Fake News Detection
+   
+  </h1>
+ <Form />
+ <Todo />
+    </div>
+   
+  );
+}
+
+export default App;
